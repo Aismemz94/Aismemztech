e87edc410bd31943e474167836f5525f07a122c4
ISAI <isai04@proton.me>
2026-02-20 03:38:13 -0400

    script.js

--- script.js
+++ script.js
@@ -5,5 +5,23 @@ function scrollToContact() {
 }
 
 function mostrarMensaje() {
-  alert("Gracias por contactarnos. Te responderemos pronto.");
+  alert("Gracias por confiar en nosotros 🚀");
 }
+
+/* Animación Reveal on Scroll */
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach(element => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const visible = 100;
+
+    if (elementTop < windowHeight - visible) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
